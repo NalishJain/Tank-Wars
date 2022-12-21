2a6145133c8b193a5e9a63132cdbaaf0470684a4
Mehar Khurana <meharkhurana03@gmail.com>
2022-12-21 23:07:54 +0530

    corrected testcases

--- core/src/com/mygdx/tankgame/testcases.java
+++ core/src/com/mygdx/tankgame/testcases.java
@@ -4,18 +4,19 @@ package com.mygdx.tankgame;
 import org.junit.Test;
 
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertEquals;
 
 public class testcases {
 
     @Test
     public void testSum1() {
         double d = Player.Distance(1,1,4,5);
-        assertSame(d,5);
+        assertEquals(d,5, 0.001f);
     }
     @Test
     public void testSum2(){
         double d =Player.Distance(1,2,9,8);
-        assertSame(d,10);
+        assertEquals(d,10, 0.001f);
     }
 
 }
